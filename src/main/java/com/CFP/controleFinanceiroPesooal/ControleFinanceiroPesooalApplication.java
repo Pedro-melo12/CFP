28b1d2ba3f115e2ccf9c705ac95a7405d6536241
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2025-04-30 13:18:43 -0300

    firt commit

---
+++ src/main/java/com/CFP/controleFinanceiroPesooal/ControleFinanceiroPesooalApplication.java
@@ -0,0 +1,13 @@
+package com.CFP.controleFinanceiroPesooal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroPesooalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroPesooalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CFP/controleFinanceiroPesooal/ControleFinanceiroPesooalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CFP.controleFinanceiroPesooal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroPesooalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
