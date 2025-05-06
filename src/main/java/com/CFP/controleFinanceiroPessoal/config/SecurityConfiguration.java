1ce2f29853ba0872ca04fd09c2b770ca5e44aae0
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2025-05-06 15:15:22 -0300

    ignorando errinhos

--- src/main/java/com/CFP/controleFinanceiroPessoal/config/SecurityConfiguration.java
+++ src/main/java/com/CFP/controleFinanceiroPessoal/config/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.CFP.controleFinanceiroPessoal.config;
 
-import com.CFP.controleFinanceiroPessoal.config.SecurityFilter; // ajuste o import se necessário
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
